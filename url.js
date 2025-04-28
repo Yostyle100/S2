f3a87d5358a3076e8d56511040fdd7e3405d9fc8
Yostyle100 <maru810210@gmail.com>
2025-04-28 14:35:57 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,8 +8,8 @@
 
 
 const urls = [
-	'https://forms.gle/mHsdr8RN2m9i8CMm8',
-	'https://forms.gle/hyg7jou9uVjaTw8R6',
-	'https://forms.gle/RDi18nNcLCir64Qz6',
-	'https://forms.gle/1wCNjc8Pxa2UJM2N9',
+	'https://forms.gle/q3vQjmvYBKhYfsd96',
+	'https://forms.gle/BXiJ3bfunJmDBCm27',
+	'https://forms.gle/SSXWW4JGcYTcaEXQ6',
+	'https://forms.gle/4W5yfQysR3v4uGUz5',
 ];
