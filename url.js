be9ecba0043a524df578342d69c8ea03eb46d431
Yostyle100 <maru810210@gmail.com>
2025-04-28 14:32:59 +0800

    url.js

---
+++ url.js
@@ -0,0 +1,15 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
+ * 2. 包起來的網址之間用逗號分隔
+ */
+
+
+const urls = [
+	'https://forms.gle/mHsdr8RN2m9i8CMm8',
+	'https://forms.gle/hyg7jou9uVjaTw8R6',
+	'https://forms.gle/RDi18nNcLCir64Qz6',
+	'https://forms.gle/1wCNjc8Pxa2UJM2N9',
+];
